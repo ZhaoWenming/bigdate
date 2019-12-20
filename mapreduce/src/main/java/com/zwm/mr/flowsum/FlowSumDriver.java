8350b873fbd3754a9843ac6c9e05f1d92cf85be0
ZhaoWenming <1163556811@qq.com>
2019-12-20 21:49:26 +0800

    增加切片

--- mapreduce/src/main/java/com/zwm/mr/flowsum/FlowSumDriver.java
+++ mapreduce/src/main/java/com/zwm/mr/flowsum/FlowSumDriver.java
@@ -4,6 +4,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.CombineTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
@@ -27,10 +28,20 @@ public class FlowSumDriver {
         // 5设置最终输出的key和value类型
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(FlowBean.class);
-        // 6设置输入输出路径
+
+        /**
+         * 6,7两个步骤没有进行调试，如果调试不通过，讲7，8注释掉即可
+         */
+        // 6果不设置InputFormat，它默认用的是TextInputFormat.class
+        //job.setInputFormatClass(CombineTextInputFormat.class);
+        // 7虚拟存储切片最大值设置4m
+        //CombineTextInputFormat.setMaxInputSplitSize(job,4194304);
+
+        // 8设置输入输出路径
         FileInputFormat.setInputPaths(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        // 7提交job
+
+        // 9提交job
         boolean result = job.waitForCompletion(true);
         System.exit(result ? 0 : 1);
     }
